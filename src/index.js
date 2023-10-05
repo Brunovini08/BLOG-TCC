6eb165d681fa0aa0d9773a62bbf9e249fb5a2b03
Bruno Vinícius <brunovinicius082005@gmail.com>
2023-10-05 16:38:38 -0300

    feat: Adicionando um novo campo a tabela user

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import express from "express"
 import dbConnection from "./config/dbConnection.js";
 import router from "./routes/user-routes.js";
 import routerBlog from "./routes/blog-routes.js";
+import cors from "cors"
+import dotenv from "dotenv/config.js"
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
 app.use("/api/user", router)
 app.use("/api/blog", routerBlog)
