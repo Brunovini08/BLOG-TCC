0d64505673804c1403168527a5a18badc17f1cd7
Bruno Vinícius <brunovinicius082005@gmail.com>
2023-10-05 17:00:21 -0300

    fix: Correções de bugs

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import dbConnection from "./config/dbConnection.js";
 import router from "./routes/user-routes.js";
 import routerBlog from "./routes/blog-routes.js";
 import cors from "cors"
-import dotenv from "dotenv/config.js"
+
 
 const app = express()
 const PORT = process.env.PORT || 3000
-
 app.use(cors())
 app.use(express.json())
 app.use("/api/user", router)
