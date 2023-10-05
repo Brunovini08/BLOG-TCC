aed60699e75825670c88d5099212a5630a039c33
Bruno Vinícius <brunovinicius082005@gmail.com>
2023-10-05 16:35:52 -0300

    feat: Adicionando um campo a tabela like

--- src/model/like.js
+++ src/model/like.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const likeSchema = mongoose.Schema({
-    blogId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Blog"
-    },
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
+    },
+    isTrue: {
+        type: Boolean,
+        default: false
     }
 })
 
